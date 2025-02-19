195e063ddee73833a6276e48d39e8977850d0d07
Matthew Anto <matthewanto@Matthews-MacBook-Pro.local>
2025-02-18 23:19:55 -0500

    Mainpage routes, disclaimer; TODO: implement write/email capabilities via Twilio API

--- app.py
+++ app.py
@@ -10,12 +10,14 @@ def homepage():
 def about():
     return render_template("about.html")
 
+@app.route('/issues')
+def issues():
+    return render_template("issues.html")
 
 
 @app.route('/favicon.ico')
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(os.path.join(app.root_path, 'static'),'favicon.ico', mimetype='image/vnd.microsoft.icon')
 
 if __name__ == "__main__":
     app.run(debug=True)
