9699f446c6c0cb26ab2d5a17118b8c3fdab4b2e9
Matthew Anto <matthewanto@Matthews-MacBook-Pro.local>
2025-02-18 22:41:08 -0500

    I want to go to bed.

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, render_template, request, redirect, url_for
+import os
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("main.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+
+@app.route('/favicon.ico')
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, 'static'),
+                               'favicon.ico', mimetype='image/vnd.microsoft.icon')
+
+if __name__ == "__main__":
+    app.run(debug=True)
